ac3bb3b3c2e5850029f057e232b50940303cb747
JacobTurner1801 <jacobmturner@gmail.com>
2023-12-16 16:29:38 +0000

    create initial repo setup

---
+++ src/runner.py
@@ -0,0 +1,11 @@
+# Author: Jacob Turner
+# Main job is to run each model on the data up until today
+# and then predict the next day's price
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
