c6533008225a8531844c807fa61c0f9aa37b25b1
Jacob Turner <jacobmturner@gmail.com>
2024-01-14 17:48:43 +0000

    checking laptop can push

--- src/runner.py
+++ src/runner.py
@@ -6,10 +6,10 @@ from keys.read_keys import read_xgb_key, read_xgb_sec
 
 def main():
     # read the api keys for xgboost and lstm accounts
-    xgb_key = read_xgb_key()
-    xgb_sec = read_xgb_sec()
+    # xgb_key = read_xgb_key()
+    # xgb_sec = read_xgb_sec()
     # print("{}, {}".format(xgb_key, xgb_sec))
-
+    print("Hello World")
 
 if __name__ == "__main__":
     main()
