cb0bde02279b41575d81104bec4ae69c8f096505
JacobTurner1801 <jacobmturner@gmail.com>
2024-01-17 10:44:01 +0000

    add getting of lstm keys

--- src/keys/read_keys.py
+++ src/keys/read_keys.py
@@ -15,16 +15,15 @@ def read_xgb_sec():
     return os.environ.get("XGB_API_SECRET")
 
 
-# TODO: Add LSTM keys
-# def read_lstm_key():
-#     """
-#     Reads the LSTM API key from the environment variable LSTM_API_KEY.
-#     """
-#     return os.environ.get("LSTM_API_KEY")
+def read_lstm_key():
+    """
+    Reads the LSTM API key from the environment variable LSTM_API_KEY.
+    """
+    return os.environ.get("LSTM_API_KEY")
 
 
-# def read_lstm_sec():
-#     """
-#     Reads the LSTM API secret from the environment variable LSTM_API_SECRET.
-#     """
-#     return os.environ.get("LSTM_API_SECRET")
+def read_lstm_sec():
+    """
+    Reads the LSTM API secret from the environment variable LSTM_API_SECRET.
+    """
+    return os.environ.get("LSTM_API_SECRET")
--- src/runner.py
+++ src/runner.py
@@ -1,15 +1,16 @@
 # Author: Jacob Turner
 # Main job is to run each model on the data up until today
 # and then predict the next day's price
-from keys.read_keys import read_xgb_key, read_xgb_sec
+from keys.read_keys import *
 from util import get_data
 
 
 def main():
     xgb_key = read_xgb_key()
     xgb_sec = read_xgb_sec()
-    # TODO: read lstm keys
-    amazon_df = get_data("AMZN", "max")
+    lstm_key = read_lstm_key()
+    lstm_sec = read_lstm_sec()
+    # amazon_df = get_data("AMZN", "max")
 
 
 if __name__ == "__main__":
