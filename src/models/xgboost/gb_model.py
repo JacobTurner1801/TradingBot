84aeb48b9afd1433c1cdd92b981065a8527134b8
JacobTurner1801 <jacobmturner@gmail.com>
2024-01-19 15:25:49 +0000

    small change to import name

--- src/models/xgboost/gb_model.py
+++ src/models/xgboost/gb_model.py
@@ -1,8 +1,8 @@
-from xgboost import *
+import xgboost as xgboost
 
 
 def train_and_fit(x_train, y_train, x_test):
-    model = XGBRegressor()
+    model = xgboost.XGBRegressor()
     model.fit(x_train, y_train)
     prediction = model.predict(x_test)
     return prediction
