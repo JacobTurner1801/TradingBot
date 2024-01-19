ee6ebd32913ca164617a26ab4ca15b7dabd10470
JacobTurner1801 <jacobmturner@gmail.com>
2024-01-19 15:26:22 +0000

    redo main to have different paths for the models, since I'll want to run one at a time probably

--- src/runner.py
+++ src/runner.py
@@ -2,15 +2,38 @@
 # Main job is to run each model on the data up until today
 # and then predict the next day's price
 from keys.read_keys import *
-from util import get_data
+from util import get_data, split_data, get_metrics_results
+from models.xgboost.gb_model import *
+
+
+def xg_path():
+    xgb_key = read_xgb_key()
+    xgb_sec = read_xgb_sec()
+    amazon_df = get_data("AMZN", "max")
+    amazon_df.dropna()
+    # create xgboost model
+    X_train, x_test, y_train, y_test = split_data(amazon_df)
+    predictions = train_and_fit(X_train, y_train, x_test)
+    df_res = get_metrics_results(y_test, predictions)
+    return df_res
+
+
+def ls_path():
+    lstm_key = read_lstm_key()
+    lstm_sec = read_lstm_sec()
+    amazon_df = get_data("AMZN", "max")
 
 
 def main():
-    xgb_key = read_xgb_key()
-    xgb_sec = read_xgb_sec()
-    lstm_key = read_lstm_key()
-    lstm_sec = read_lstm_sec()
-    # amazon_df = get_data("AMZN", "max")
+    inp = int(input("Enter 1 for XGBoost, 2 for LSTM: "))
+    if inp == 1:
+        df = xg_path()
+        print(df)
+    elif inp == 2:
+        ls_path()
+    else:
+        print("Invalid input")
+    return 0
 
 
 if __name__ == "__main__":
