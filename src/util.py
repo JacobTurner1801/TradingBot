95a5418d11bdcf7dbff71ab306f615b0c4d83a09
JacobTurner1801 <jacobmturner@gmail.com>
2024-01-19 15:25:37 +0000

    add metrics

--- src/util.py
+++ src/util.py
@@ -1,6 +1,12 @@
 from yfinance import *
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score,
+)
+import numpy as np
 
 
 def get_data(ticker, period) -> pd.DataFrame:
@@ -8,8 +14,34 @@ def get_data(ticker, period) -> pd.DataFrame:
     return data
 
 
-def split_data(data: pd.DataFrame):
-    x_train, y_train, x_test, y_test = train_test_split(
-        data, train_size=0.7, test_size=0.3
+def symetric_mape(y_test, predictions):
+    constant = 100 / len(y_test)
+    return constant * np.sum(
+        2 * np.abs(predictions - y_test) / (np.abs(y_test) + np.abs(predictions))
     )
-    return x_train, y_train, x_test, y_test
+
+
+def split_data(data: pd.DataFrame):
+    y = data["Close"]
+    X = data.drop(["Close"], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=1
+    )
+    return X_train, X_test, y_train, y_test
+
+
+def make_frame(metrics):
+    frame = pd.DataFrame()
+    frame["MAE"] = [metrics[0]]
+    frame["RMSE"] = [metrics[1]]
+    frame["SMAPE"] = [metrics[2]]
+    frame["R2"] = [metrics[3]]
+    return frame
+
+
+def get_metrics_results(y_test, predictions):
+    mae = mean_absolute_error(y_test, predictions)
+    rmse = np.sqrt(mean_squared_error(y_test, predictions))
+    smape = symetric_mape(y_test, predictions)
+    r2 = r2_score(y_test, predictions)
+    return make_frame([mae, rmse, smape, r2])
