94a42adf0984cd5fd17c59975675989d77e6cc66
Jacob Turner <jacobmturner@gmail.com>
2024-02-13 11:35:08 +0000

    change docs to come up when hovering over function

--- src/models/xgboost/gb_model.py
+++ src/models/xgboost/gb_model.py
@@ -3,31 +3,31 @@ import numpy as np
 import pandas as pd
 from datetime import timedelta
 
-"""
-Create xgboost model without using bayesian optimisation
-@return model
-"""
 def create_model(n_est=1000, lr=0.01, md=5):
+    """
+    Create xgboost model without using bayesian optimisation
+    @return model
+    """
     model = xgboost.XGBRegressor(
         n_estimators=n_est, learning_rate=lr, max_depth=md, random_state=1
     )
     return model
 
-"""
-train and run xgboost model on a split dataset
-@return predictions
-"""
 def train_and_fit(x_train, y_train, x_test):
+    """
+    train and run xgboost model on a split dataset
+    @return predictions
+    """
     model = create_model()
     model.fit(x_train, y_train)
     prediction = model.predict(x_test)
     return prediction
 
-"""
-Generate 5 day predictions using xgboost model
-@return nothing, just prints the predictions
-"""
 def generate_five_day_predictions_xgb(df: pd.DataFrame):
+    """
+    Generate 5 day predictions using xgboost model
+    @return nothing, just prints the predictions
+    """
     df = df.dropna()
     df = df.sort_index()
     # lets only work with time series data then I can refactor later
