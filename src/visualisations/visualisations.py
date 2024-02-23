0ec0af48d7637ee6ef8cdb175e08781819ca4d76
JacobTurner1801 <jacobmturner@gmail.com>
2024-02-23 16:04:57 +0000

    merge metrics function and file

--- src/visualisations/visualisations.py
+++ src/visualisations/visualisations.py
@@ -1,7 +1,9 @@
+import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-import numpy as np
+
+# import numpy as np
 
 
 def plot_predictions(preds: pd.DataFrame, actuals: pd.DataFrame):
@@ -41,3 +43,27 @@ def create_metrics_table(metrics: pd.DataFrame):
     @return table: DataFrame
     """
     return pd.plotting.table(data=metrics, loc="center", colWidths=[0.2, 0.2, 0.2, 0.2])
+
+
+def merge_metrics_files(path: str):
+    """
+    Merge metrics files from a given path (in this case the ../metrics_mvp folder)
+    """
+    # get the contents of the directory
+    files = os.listdir(path)
+    # filter for csv files
+    files = [file for file in files if file.endswith(".csv")]
+    # read in all the files as csv
+    dfs = [pd.read_csv(path + file) for file in files]
+    # concatenate the dataframes
+    df = pd.concat(dfs)
+    # drop the index column
+    df = df.drop(columns=["Unnamed: 0"])
+    # save the file
+    df.to_csv(path + "merged_metrics.csv")
+    return df
+
+
+# CHANGE THIS PATH FOR THE CSV FILES TO MERGE
+df = merge_metrics_files("../../metrics_mvp/xgb/")
+print(df)
